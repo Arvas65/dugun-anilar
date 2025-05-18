ccca3f64c033d43ad54cc4b5b060c662400b2823
Arvas65 <58606798+Arvas65@users.noreply.github.com>
2025-05-19 00:15:33 +0300

    Update cloudinary_upload.py

--- cloudinary_upload.py
+++ cloudinary_upload.py
@@ -3,9 +3,9 @@ import cloudinary.uploader
 import os
 
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+    cloud_name='djjy2cqpc',
+    api_key='336236266214479',
+    api_secret='pPeUaKJuGtNuWh2VfT0Zon21nSQ'
 )
 
 def upload_to_cloudinary(local_path, resource_type="auto"):
